7f9d81a9b81b2fced6f69deb6d4e389f53776ed8
nelson <azees1995@gmail.com>
2024-12-16 12:30:37 +0500

    Update client-components.ts

---
+++ src/components/client-components.ts
@@ -0,0 +1,6 @@
+'use client'
+
+export { default as Chat } from './Chat'
+export { default as Sidebar } from './Sidebar'
+export { default as SignIn } from './SignIn'
+export { default as WelcomeScreen } from './WelcomeScreen'
