80fd9130db4901fc234558b822ef506d8134826a
nelson <azees1995@gmail.com>
2024-12-16 09:54:23 +0500

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,7 +24,7 @@ interface SidebarProps {
   toggleDarkMode: () => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ open, onClose, darkMode, toggleDarkMode }) => {
+export const Sidebar: React.FC<SidebarProps> = ({ open, onClose, darkMode, toggleDarkMode }) => {
   const { user, signOut } = useAuth();
 
   return (
