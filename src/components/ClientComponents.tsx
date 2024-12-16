956f22a66b8c62be4536e30ec8e5f752b02ab2e9
nelson <azees1995@gmail.com>
2024-12-16 12:29:46 +0500

    Delete src/components/ClientComponents.tsx

--- src/components/ClientComponents.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-export { default as Chat } from './Chat'
-export { default as Sidebar } from './Sidebar'
-export { default as SignIn } from './SignIn'
-export { default as WelcomeScreen } from './WelcomeScreen'
-
