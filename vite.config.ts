335bf99532d36efbc06534f7a0b180acb7e08834
nelson <azees1995@gmail.com>
2024-12-16 12:35:30 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           mui: ['@mui/material', '@mui/icons-material'],
-          emotion: ['@emotion/react', '@emotion/styled'],
         },
       },
     },
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', '@mui/material', '@emotion/react', '@emotion/styled'],
+    include: ['react', 'react-dom', 'react-router-dom', '@mui/material', '@mui/icons-material'],
   },
 });
+
